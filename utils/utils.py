0b4292a51837271b9297b98d0744a347089ae593
cesareressa <cesare.ressa@gmail.com>
2023-04-14 14:06:36 +0200

    wip

--- utils/utils.py
+++ utils/utils.py
@@ -164,31 +164,31 @@ def generate_plots(movies_df: pd.DataFrame, genre: str, decade: int, top: int = 
     plt.show
 
 
-def find_closest_match_ut(genre, prod_year, dataset):
-    """
-    Compare a proposed new movie against similar movies in the dataset and
-    visualize how the similar movies behaved.
+# def find_closest_match_ut(genre, prod_year, dataset):
+#     """
+#     Compare a proposed new movie against similar movies in the dataset and
+#     visualize how the similar movies behaved.
 
-    Args:
-        genre (str): Genre of the proposed new movie.
-        prod_year (int): Production year of the proposed new movie.
-        dataset (pd.DataFrame): Dataset of movies to compare against.
+#     Args:
+#         genre (str): Genre of the proposed new movie.
+#         prod_year (int): Production year of the proposed new movie.
+#         dataset (pd.DataFrame): Dataset of movies to compare against.
 
-    Returns:
-        filter_df (pd.DataFrame): Filtered dataframe per genre and decade.
-        aggregate_df (pd.DataFrame): Aggregated data for visualization.
-    """
+#     Returns:
+#         filter_df (pd.DataFrame): Filtered dataframe per genre and decade.
+#         aggregate_df (pd.DataFrame): Aggregated data for visualization.
+#     """
 
-    # Define decade in which the proposed movie has been produced
-    decade = prod_year // 10 * 10
+#     # Define decade in which the proposed movie has been produced
+#     decade = prod_year // 10 * 10
 
-    # filter df per start and end decade (+9 yrs)
-    filter_df = filter_production_date(dataset, decade, decade + 9)
-    filter_df = filter_df.explode("genres")
-    filter_df = filter_df[filter_df["genres"].str.contains(genre)]
+#     # filter df per start and end decade (+9 yrs)
+#     filter_df = filter_production_date(dataset, decade, decade + 9)
+#     filter_df = filter_df.explode("genres")
+#     filter_df = filter_df[filter_df["genres"].str.contains(genre)]
 
-    aggregate_df = aggregate_data(filter_df)
+#     aggregate_df = aggregate_data(filter_df)
 
-    generate_plots(aggregate_df, genre, decade)
+#     generate_plots(aggregate_df, genre, decade)
 
-    return filter_df, aggregate_df
+#     return filter_df, aggregate_df
